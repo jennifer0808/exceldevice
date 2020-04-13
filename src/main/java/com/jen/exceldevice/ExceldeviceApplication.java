0afb1d176a516a68c6e2856e280a8b9786183cfb
jennifer0808 <wj_computer@outlook.com>
2020-04-13 14:20:19 +0800

    new create project

---
+++ src/main/java/com/jen/exceldevice/ExceldeviceApplication.java
@@ -0,0 +1,13 @@
+package com.jen.exceldevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceldeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceldeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jen/exceldevice/ExceldeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jen.exceldevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceldeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
